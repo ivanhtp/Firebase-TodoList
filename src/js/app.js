415ebe7e826d6e35cbdbb128b5b0a5dae5f1c810
Ivan Pauletti <ivanpauletti@gmail.com>
2016-08-31 16:30:24 -0300

    Firebase up and running (no auth yet)

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,35 @@
-// Include app dependency on ngMaterial
+'use strict';
 
+/**
+ * Module declaration
+ */
 var app = angular.module( 'app', [ 'ngMaterial' ] );
 
+/**
+ * Main controller
+ * @dependency $scope
+ */
 app.controller("MainController", [ '$scope',
     function ($scope){
         $scope.todo = "testando";
     }
 ]);
+
+
+/**
+ * Run directly after load
+ */
+app.run([ function (){
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBCXSqopJcZXxi4t5x_FPdBkZytzye7QcM",
+        authDomain: "todo-list-a20d9.firebaseapp.com",
+        databaseURL: "https://todo-list-a20d9.firebaseio.com",
+        storageBucket: "todo-list-a20d9.appspot.com",
+    };
+    firebase.initializeApp(config);
+
+
+
+}]);
