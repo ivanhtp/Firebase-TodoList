abfdb558c9db886b9a3743e92a5c82ca56da18f5
Ivan Pauletti <ivanpauletti@gmail.com>
2016-09-01 15:14:41 -0300

    Authentication and loading data via angularfire

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,35 @@
 /**
  * Module declaration
  */
-var app = angular.module( 'app', [ 'ngMaterial' ] );
+var app = angular.module( 'app', [ 'ngMaterial', 'firebase' ] );
 
 /**
  * Main controller
  * @dependency $scope
  */
-app.controller("MainController", [ '$scope',
-    function ($scope){
-        $scope.todo = "testando";
+app.controller("MainController", [ '$scope', '$firebaseObject', '$firebaseAuth',
+    function ($scope, $firebaseObject, $firebaseAuth){
+        var ref = firebase.database().ref();
+
+        // create an instance of the authentication service
+        var auth = $firebaseAuth();
+        console.log(auth);
+
+        // login with Facebook
+        auth.$signInWithEmailAndPassword("ivanpauletti@gmail.com", 'abc001').then(function(authData) {
+            console.log("Logged in as:", authData.uid);
+
+            // download the data into a local object
+            var syncObject = $firebaseObject(ref);
+
+            // synchronize the object with a three-way data binding
+            // click on `index.html` above to see it used in the DOM!
+            syncObject.$bindTo($scope, "todo");
+        }).catch(function(error) {
+            console.log("Authentication failed:", error);
+        });
+
+
     }
 ]);
 
@@ -29,7 +49,6 @@ app.run([ function (){
         storageBucket: "todo-list-a20d9.appspot.com",
     };
     firebase.initializeApp(config);
-
 
 
 }]);
